ff525f27b5241b10038db8ebb5a297ebf7a5eafd
royweb <xytank@gmail.com>
2020-05-19 15:34:32 -0400

    Create the random blocks

--- Assets/Scripts/BlockSpawner.cs
+++ Assets/Scripts/BlockSpawner.cs
@@ -5,10 +5,23 @@ public class BlockSpawner : MonoBehaviour
     // [] for array
     public Transform[] spawnPoints;
 
+    public GameObject blockPrefab;
+
     // Start is called before the first frame update
     void Start()
     {
         // Get a random number
         int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for(int i = 0; i < spawnPoints.Length; i++)
+        {
+            if(randomIndex != i)
+            {
+                // Create the block
+                // Quaternion.identity to prevent rotate
+                Intantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+            
+        }
     }
 }
