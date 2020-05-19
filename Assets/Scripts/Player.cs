3437f7596fc8f5476f6a3ca38507dd60de081e35
royweb <xytank@gmail.com>
2020-05-19 10:08:27 -0400

    Allow the movement of the player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,14 @@ public class Player : MonoBehaviour
 {
     public float speed = 15f;
 
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        // Get access to the Rigidbody2D on unity
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     // Good for physics stuff because it is run on a fixed timer
     void FixedUpdate()
     {
@@ -11,5 +19,9 @@ public class Player : MonoBehaviour
 
         // For left and right movement
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+
+        // Move the player
+        // rb.position is the current position of the player
+        rb.MovePosition(rb.position + Vector2.right * x);
     }
 }
