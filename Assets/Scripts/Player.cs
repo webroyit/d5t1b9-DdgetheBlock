bd342ccc235cef3ad12aeaee9b938df06b745481
royweb <xytank@gmail.com>
2020-05-19 10:31:23 -0400

    Prevent the player from moving out of range

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,6 +3,7 @@
 public class Player : MonoBehaviour
 {
     public float speed = 15f;
+    public float mapWidth = 5f;
 
     private Rigidbody2D rb;
 
@@ -20,8 +21,15 @@ public class Player : MonoBehaviour
         // For left and right movement
         float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
 
-        // Move the player
         // rb.position is the current position of the player
-        rb.MovePosition(rb.position + Vector2.right * x);
+        Vector2 newPosition = rb.position + Vector2.right * x;
+
+        // Make sure that the player does not move out of range
+        // Second argument is the min value
+        // Third argument is the max value
+        newPosition.x = Mathf.Clamp(newPosition.x, -mapWidth, mapWidth);
+
+        // Move the player
+        rb.MovePosition(newPosition);
     }
 }
