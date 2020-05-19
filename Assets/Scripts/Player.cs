a67cb44bf3431e4725df0d74d3033b727f006b19
royweb <xytank@gmail.com>
2020-05-19 09:47:59 -0400

    Use Input.GetAxis() for left and right movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,5 +2,14 @@
 
 public class Player : MonoBehaviour
 {
-    
+    public float speed = 15f;
+
+    // Good for physics stuff because it is run on a fixed timer
+    void FixedUpdate()
+    {
+        // Note: Add Rigidbody 2D component to the player and set the Body Type to kinematic
+
+        // For left and right movement
+        float x = Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed;
+    }
 }
