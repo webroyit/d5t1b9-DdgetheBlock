d7f07480a467d85728a35cb1de88baf3ef9bffb3
royweb <xytank@gmail.com>
2020-05-21 13:16:37 -0400

    Use IEnumerator to delay on restarting the game

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public void EndGame()
     {
-        // Restart the game
+        // StartCoroutine to work with IEnumerator
+        StartCoroutine(RestartLevel());
+    }
+
+    // Deplay on restarting the game
+    IEnumerator RestartLevel()
+    {
+        // Wait 1 second
+        yield return new WaitForSeconds(1f);
+
+         // Restart the game
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
