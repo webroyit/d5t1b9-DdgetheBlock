bc565d6a0c8b70adaf9e256504caf1fa23502fe5
royweb <xytank@gmail.com>
2020-05-21 14:26:16 -0400

    Increase the gravity of the block per second

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -2,6 +2,13 @@
 
 public class Block : MonoBehaviour
 {
+    void Start()
+    {
+        // Access Rigidbody2D gravity scale property on Unity
+        // Time.timeSinceLevelLoad to update the gravity of the block for certain time for this level
+        GetComponent<Rigidbody2D>().gravityScale += Time.timeSinceLevelLoad / 20f;
+    }
+
     // Update is called once per frame
     void Update()
     {
