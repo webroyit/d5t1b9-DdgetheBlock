b2be37fbe95fa5537a9dbf1bf2d4617c90517e65
royweb <xytank@gmail.com>
2020-05-21 10:58:20 -0400

    Clear unused Blocks

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -5,6 +5,11 @@ public class Block : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Remove the Block if it under the player
+        if(transform.position.y < -2f)
+        {
+            // Remove the Block
+            Destroy(gameObject);
+        }
     }
 }
